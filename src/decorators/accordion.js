a4cb2c5731e30808cfc53fcf389e0053d38c4649
Roman <kaboom.ua@gmail.com>
2016-10-03 22:19:04 +0300

    Update accordion.js

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function accordion(Component) {
     return class WrappedComponent extends React.Component {
         state = {
+            //Не привязывайся к названию сущности, декоратор будет использоваться везде. Назови, скажем, openItemId
             openArticleId: null
         }
 
@@ -11,6 +12,7 @@ export default function accordion(Component) {
         }
 
         toggleOpenArticle = id => ev => {
+            //да, но можно было написать короче тернарным оператором
             if( id == this.state.openArticleId ) {
                 this.setState({
                     openArticleId: null
