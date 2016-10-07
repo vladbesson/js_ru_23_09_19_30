fdb6f167d5a0f61088f01a981b49b05c43152f90
Roman <kaboom.ua@gmail.com>
2016-10-07 10:50:00 +0300

    Update NewArticleForm.js

--- src/components/NewArticleForm.js
+++ src/components/NewArticleForm.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import DayPicker, { DateUtils } from 'react-day-picker'
 
 import 'react-day-picker/lib/style.css'
+//Почему календарь здесь? еле нашел)
 
 function sunday(day) {
     return day.getDay() === 0;
